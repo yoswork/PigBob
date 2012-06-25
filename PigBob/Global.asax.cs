3fdd03eee050be0430dc38da32b39031341f4dc4
Yos Noor <yusuf.noor@reed.co.uk>
2012-06-25 15:22:30 +0100

    Fourth Version
    The Fourth Version

--- PigBob/Global.asax.cs
+++ PigBob/Global.asax.cs
@@ -4,11 +4,13 @@ using System.Configuration;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace PigBob
 {
@@ -21,9 +23,16 @@ namespace PigBob
         {
             AreaRegistration.RegisterAllAreas();
 
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("Mobile")
+            {
+                ContextCondition = Context => Context.Request.Browser.Platform == "Android"
+            });
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
         }
     }
 }
